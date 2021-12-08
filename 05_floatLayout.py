53350a00df54e7634adb790574d730ff3f276bd1
tayyabmalik4 <mtayyabmalik99@gmail.com>
2021-12-08 14:15:09 +0500

    floatLayout

---
+++ 05_floatLayout.py
@@ -0,0 +1,11 @@
+import kivy
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+
+class floatApp(App):
+    def build(self):
+        return FloatLayout()
+
+
+if __name__ == "__main__":
+    floatApp().run()
